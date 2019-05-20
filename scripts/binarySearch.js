7099320c80def67a548650ad1298e072be75625f
Tyler Hawkins <thawkins@instructure.com>
2019-05-20 14:29:00 -0600

    modified linearSearch; added binarySearch

---
+++ scripts/binarySearch.js
@@ -0,0 +1,39 @@
+// Binary Search
+// Only can be used for sorted arrays
+// Best case: 0(1) (the element you're looking for is located at the array's midpoint)
+// Worst case: 0(log n) (the element is located near the beginning or end of the array)
+
+const binarySearch = (haystack, needle, showLogs) => {
+    let searchableHaystack = [...haystack];
+    let i = 1;
+    let fullHaystackMidpointIndex = 0;
+
+    while (searchableHaystack.length > 0) {
+        const midpointIndex = Math.floor(searchableHaystack.length / 2);
+        fullHaystackMidpointIndex += midpointIndex;
+        showLogs && console.log(`iteration ${i}: midpoint index: ${fullHaystackMidpointIndex}; array to search: ${searchableHaystack.join(', ')}; ${needle} === ${searchableHaystack[midpointIndex]} ? ... ${needle === searchableHaystack[midpointIndex]}!`);
+
+        if (searchableHaystack[midpointIndex] === needle) {
+            return fullHaystackMidpointIndex;
+        } else if (searchableHaystack[midpointIndex] > needle) {
+            fullHaystackMidpointIndex -= midpointIndex;
+            searchableHaystack = searchableHaystack.slice(0, midpointIndex);
+        } else {
+            fullHaystackMidpointIndex += 1;
+            searchableHaystack = searchableHaystack.slice(midpointIndex + 1);
+        }
+
+        i++;
+    }
+    return -1;
+}
+
+const binarySearchTest1 = [1, 3, 5, 8, 9, 10, 12];
+const binarySearchTest2 = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30];
+
+console.log(binarySearch(binarySearchTest1, 10, true))
+console.log(binarySearch(binarySearchTest1, 999, true))
+console.log(binarySearch(binarySearchTest2, 14, true))
+console.log(binarySearch(binarySearchTest2, 0, true))
+console.log(binarySearch(binarySearchTest2, 22, true))
+console.log(binarySearch(binarySearchTest2, 999, true))
--- scripts/linearSearch.js
+++ scripts/linearSearch.js
@@ -1,17 +1,21 @@
 // Linear Search
+// Best case: 0(1) (the element you're looking for is the first in the array)
+// Worst case: 0(n) (the element you're looking for is the last in the array or not in the array at all)
+
 const linearSearch = (haystack, needle, showLogs) => {
-	for (let i = 0; i < haystack.length; i++) {
-		if (needle === haystack[i]) {
-			return i;
-		}
-	}
-	return -1;
+    for (let i = 0; i < haystack.length; i++) {
+        showLogs && console.log(`iteration ${i + 1}: ${needle} === ${haystack[i]} ? ... ${needle === haystack[i]}!`);
+        if (needle === haystack[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 const linearSearchTest1 = [1, 3, 8, 5, 9, 10, 12];
 const linearSearchTest2 = ['c', 'b', 'd', 'c'];
 
-console.log(linearSearch(linearSearchTest1, 8))
-console.log(linearSearch(linearSearchTest1, 100))
-console.log(linearSearch(linearSearchTest2, 'c'))
-console.log(linearSearch(linearSearchTest2, 'a'))
+console.log(linearSearch(linearSearchTest1, 8, true))
+console.log(linearSearch(linearSearchTest1, 100, true))
+console.log(linearSearch(linearSearchTest2, 'c', true))
+console.log(linearSearch(linearSearchTest2, 'a', true))
