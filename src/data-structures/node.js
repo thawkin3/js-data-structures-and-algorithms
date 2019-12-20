86143a75a20140bbdc4dc9b1924b305f11fa14f4
Tyler Hawkins <thawkins@instructure.com>
2019-12-20 09:56:22 -0700

    adds documentation for node

--- src/data-structures/node.js
+++ src/data-structures/node.js
@@ -1,3 +1,12 @@
+/**
+ * Node - Contains a value and a pointer to the next node
+ *
+ * Methods and properties:
+ *
+ * - val: any value
+ * - next: pointer to the next node (or null)
+ */
+
 export class Node {
   constructor(val, next = null) {
     this.val = val
