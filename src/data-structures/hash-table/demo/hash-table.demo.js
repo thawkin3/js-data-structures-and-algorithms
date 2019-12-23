466699d1a7f79bcad532045d0271583f01f2b0e4
Tyler Hawkins <thawkins@instructure.com>
2019-12-23 14:40:03 -0700

    adds a placeholder demo for hash table

---
+++ src/data-structures/hash-table/demo/hash-table.demo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default {
+  title: 'Data Structures|Hash Table',
+}
+
+export const readTheDocs = () => (
+  <div>
+    <p>Objects in JavaScript are hash tables!</p>
+    <p>
+      Go read the docs here:{' '}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object"
+      >
+        MDN - Object
+      </a>
+    </p>
+  </div>
+)
