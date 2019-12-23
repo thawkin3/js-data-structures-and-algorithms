c1e2ab2fc9541d62877e9c0cddef5590036165dd
Tyler Hawkins <thawkins@instructure.com>
2019-12-23 14:38:20 -0700

    added placeholder demo for Array

---
+++ src/data-structures/array/demo/array.demo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default {
+  title: 'Data Structures|Array',
+}
+
+export const readTheDocs = () => (
+  <div>
+    <p>Arrays are already implemented in JavaScript!</p>
+    <p>
+      Go read the docs here:{' '}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array"
+      >
+        MDN - Array
+      </a>
+    </p>
+  </div>
+)
