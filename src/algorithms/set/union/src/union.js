3986c9d1b205af62bbc3d6de993cb471373139a9
Tyler Hawkins <thawkins@instructure.com>
2020-01-02 13:40:33 -0700

    adds set union method

--- src/algorithms/set/union/src/union.js
+++ src/algorithms/set/union/src/union.js
@@ -1,6 +1,13 @@
 // Union
-// TODO: add info here
+// Compares two sets and produces a third set that contains all unique values found in either set
+// Ex. Union of { 1, 2, 3 } and { 3, 4, 5 } is { 1, 2, 3, 4, 5 }
+// Performance: Linear - O(n + m), where n is the length of set 1 and m is the length of set 2
+
+import { Set } from '../../../../data-structures/set/src/set'
 
 export const union = (set1, set2) => {
-  // TODO: implement union
+  const unionSet = new Set()
+  set1.enumerate().forEach(val => unionSet.add(val))
+  set2.enumerate().forEach(val => unionSet.add(val))
+  return unionSet
 }
--- src/algorithms/set/union/src/union.test.js
+++ src/algorithms/set/union/src/union.test.js
@@ -1,6 +1,47 @@
 import { union } from './union'
+import { Set } from '../../../../data-structures/set/src/set'
 
 describe('intersection', () => {
-  // TODO
-  it('passes', () => {})
+  it('creates a third set from the two given sets', () => {
+    const set1 = new Set()
+    set1.add(42)
+    set1.add(10)
+
+    const set2 = new Set()
+    set2.add('a')
+    set2.add('b')
+    set2.add('c')
+
+    const set3 = union(set1, set2)
+
+    expect(set3.size()).toBe(5)
+    expect(set3.enumerate()).toEqual([42, 10, 'a', 'b', 'c'])
+  })
+
+  it('only adds unique values', () => {
+    const set1 = new Set()
+    set1.add(42)
+    set1.add(10)
+    set1.add('a')
+
+    const set2 = new Set()
+    set1.add(42)
+    set2.add('a')
+    set2.add('b')
+    set2.add('c')
+
+    const set3 = union(set1, set2)
+
+    expect(set3.size()).toBe(5)
+    expect(set3.enumerate()).toEqual([42, 10, 'a', 'b', 'c'])
+  })
+
+  it('can handle creating the union of two empty sets', () => {
+    const set1 = new Set()
+    const set2 = new Set()
+    const set3 = union(set1, set2)
+
+    expect(set3.size()).toBe(0)
+    expect(set3.enumerate()).toEqual([])
+  })
 })
