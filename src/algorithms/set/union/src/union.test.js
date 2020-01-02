52bf7356c1ccfc9a351e6d5f6c90ca82c600753d
Tyler Hawkins <thawkins@instructure.com>
2020-01-02 13:54:13 -0700

    fixes typo, corrects 'intersection' to 'union'

--- src/algorithms/set/union/src/union.test.js
+++ src/algorithms/set/union/src/union.test.js
@@ -1,7 +1,7 @@
 import { union } from './union'
 import { Set } from '../../../../data-structures/set/src/set'
 
-describe('intersection', () => {
+describe('union', () => {
   it('creates a third set from the two given sets', () => {
     const set1 = new Set()
     set1.add(42)
@@ -34,6 +34,28 @@ describe('intersection', () => {
 
     expect(set3.size()).toBe(5)
     expect(set3.enumerate()).toEqual([42, 10, 'a', 'b', 'c'])
+  })
+
+  it('does not modify the original two sets', () => {
+    const set1 = new Set()
+    set1.add(42)
+    set1.add(10)
+
+    const set2 = new Set()
+    set2.add('a')
+    set2.add('b')
+    set2.add('c')
+
+    const set3 = union(set1, set2)
+
+    expect(set3.size()).toBe(5)
+    expect(set3.enumerate()).toEqual([42, 10, 'a', 'b', 'c'])
+
+    expect(set1.size()).toBe(2)
+    expect(set1.enumerate()).toEqual([42, 10])
+
+    expect(set2.size()).toBe(3)
+    expect(set2.enumerate()).toEqual(['a', 'b', 'c'])
   })
 
   it('can handle creating the union of two empty sets', () => {
