05bc035053289dfffc9b4471f1e274530f3e7d0b
Tyler Hawkins <thawkins@instructure.com>
2020-01-02 13:54:30 -0700

    adds set intersection method

--- src/algorithms/set/intersection/src/intersection.js
+++ src/algorithms/set/intersection/src/intersection.js
@@ -1,6 +1,26 @@
 // Intersection
-// TODO: add info here
+// Compares two sets and produces a third set that contains all of the
+// intersecting/matching values that are found in both sets
+// Ex. Intersection of { 1, 2, 3 } and { 2, 3, 4 } is { 2, 3 }
+// Performance: Quadratic - O(n * m), where n is the length of set 1 and m is the length of set 2
+// Note: this is sort of like O(n^2) performance
+
+import { Set } from '../../../../data-structures/set/src/set'
 
 export const intersection = (set1, set2) => {
-  // TODO: implement intersection
+  const intersectionSet = new Set()
+
+  set1.enumerate().forEach(val => {
+    if (set2.has(val)) {
+      intersectionSet.add(val)
+    }
+  })
+
+  set2.enumerate().forEach(val => {
+    if (set1.has(val)) {
+      intersectionSet.add(val)
+    }
+  })
+
+  return intersectionSet
 }
--- src/algorithms/set/intersection/src/intersection.test.js
+++ src/algorithms/set/intersection/src/intersection.test.js
@@ -1,6 +1,71 @@
 import { intersection } from './intersection'
+import { Set } from '../../../../data-structures/set/src/set'
 
 describe('intersection', () => {
-  // TODO
-  it('passes', () => {})
+  it('creates a third set from the two given sets, using only values that are present in both sets', () => {
+    const set1 = new Set()
+    set1.add(42)
+    set1.add(10)
+    set1.add('a')
+
+    const set2 = new Set()
+    set2.add(42)
+    set2.add('a')
+    set2.add('b')
+    set2.add('c')
+
+    const set3 = intersection(set1, set2)
+
+    expect(set3.size()).toBe(2)
+    expect(set3.enumerate()).toEqual([42, 'a'])
+  })
+
+  it('only adds values that are present in both sets', () => {
+    const set1 = new Set()
+    set1.add(42)
+    set1.add(10)
+
+    const set2 = new Set()
+    set2.add('a')
+    set2.add('b')
+    set2.add('c')
+
+    const set3 = intersection(set1, set2)
+
+    expect(set3.size()).toBe(0)
+    expect(set3.enumerate()).toEqual([])
+  })
+
+  it('does not modify the original two sets', () => {
+    const set1 = new Set()
+    set1.add(42)
+    set1.add(10)
+    set1.add('a')
+
+    const set2 = new Set()
+    set2.add(42)
+    set2.add('a')
+    set2.add('b')
+    set2.add('c')
+
+    const set3 = intersection(set1, set2)
+
+    expect(set3.size()).toBe(2)
+    expect(set3.enumerate()).toEqual([42, 'a'])
+
+    expect(set1.size()).toBe(3)
+    expect(set1.enumerate()).toEqual([42, 10, 'a'])
+
+    expect(set2.size()).toBe(4)
+    expect(set2.enumerate()).toEqual([42, 'a', 'b', 'c'])
+  })
+
+  it('can handle creating the intersection of two empty sets', () => {
+    const set1 = new Set()
+    const set2 = new Set()
+    const set3 = intersection(set1, set2)
+
+    expect(set3.size()).toBe(0)
+    expect(set3.enumerate()).toEqual([])
+  })
 })
