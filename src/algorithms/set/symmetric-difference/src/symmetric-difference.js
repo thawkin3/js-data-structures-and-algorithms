cb58528a9cbd27274e559a908d84048bd93e213d
Tyler Hawkins <thawkins@instructure.com>
2020-01-02 14:16:54 -0700

    adds the set symmetric difference method

--- src/algorithms/set/symmetric-difference/src/symmetric-difference.js
+++ src/algorithms/set/symmetric-difference/src/symmetric-difference.js
@@ -1,6 +1,16 @@
 // Symmetric Difference
-// TODO: add info here
+// Compares two sets and returns all of the items that exist in only one of the two sets
+// The symmetric difference is the set difference of the union and intersection of the input sets
+// Ex. Symmetric difference of { 2, 3, 4 } and { 3, 4, 5 } is { 2, 5 }
+// Performance: Quadratic - O(n * m), where n is the length of set 1 and m is the length of set 2
+// Note: this is sort of like O(n^2) performance
+
+import { setDifference } from '../../set-difference/src/set-difference'
+import { intersection } from '../../intersection/src/intersection'
+import { union } from '../../union/src/union'
 
 export const symmetricDifference = (set1, set2) => {
-  // TODO: implement symmetricDifference
+  const intersectionSet = intersection(set1, set2)
+  const unionSet = union(set1, set2)
+  return setDifference(unionSet, intersectionSet)
 }
--- src/algorithms/set/symmetric-difference/src/symmetric-difference.test.js
+++ src/algorithms/set/symmetric-difference/src/symmetric-difference.test.js
@@ -1,6 +1,73 @@
 import { symmetricDifference } from './symmetric-difference'
+import { Set } from '../../../../data-structures/set/src/set'
 
 describe('symmetricDifference', () => {
-  // TODO
-  it('passes', () => {})
+  it('creates a third set from the two given sets, using only values that are present in only one of the sets', () => {
+    const set1 = new Set()
+    set1.add(2)
+    set1.add(3)
+    set1.add(4)
+
+    const set2 = new Set()
+    set2.add(3)
+    set2.add(4)
+    set2.add(5)
+
+    const set3 = symmetricDifference(set1, set2)
+
+    expect(set3.size()).toBe(2)
+    expect(set3.enumerate()).toEqual([2, 5])
+  })
+
+  it('does not modify the original two sets', () => {
+    const set1 = new Set()
+    set1.add(2)
+    set1.add(3)
+    set1.add(4)
+
+    const set2 = new Set()
+    set2.add(3)
+    set2.add(4)
+    set2.add(5)
+
+    const set3 = symmetricDifference(set1, set2)
+
+    expect(set3.size()).toBe(2)
+    expect(set3.enumerate()).toEqual([2, 5])
+
+    expect(set1.size()).toBe(3)
+    expect(set1.enumerate()).toEqual([2, 3, 4])
+
+    expect(set2.size()).toBe(3)
+    expect(set2.enumerate()).toEqual([3, 4, 5])
+  })
+
+  it('can handle creating the symmetricDifference of two empty sets', () => {
+    const set1 = new Set()
+    const set2 = new Set()
+    const set3 = symmetricDifference(set1, set2)
+
+    expect(set3.size()).toBe(0)
+    expect(set3.enumerate()).toEqual([])
+  })
+
+  it('returns the same set of items, regardless of the order the sets are provided to the method', () => {
+    const set1 = new Set()
+    set1.add(2)
+    set1.add(3)
+    set1.add(4)
+
+    const set2 = new Set()
+    set2.add(3)
+    set2.add(4)
+    set2.add(5)
+
+    const set3 = symmetricDifference(set1, set2)
+    expect(set3.size()).toBe(2)
+    expect(set3.enumerate()).toEqual([2, 5])
+
+    const set4 = symmetricDifference(set2, set1)
+    expect(set4.size()).toBe(2)
+    expect(set4.enumerate()).toEqual([5, 2])
+  })
 })
