eedd23edf3f65c6099b0f388040f38c20f51a9c3
Tyler Hawkins <thawkins@instructure.com>
2020-01-02 21:30:08 -0700

    adds a demo for the set

--- src/data-structures/set/demo/set.demo.js
+++ src/data-structures/set/demo/set.demo.js
@@ -1,11 +1,120 @@
-import React from 'react'
+import React, { Component, createRef } from 'react'
+import { Set } from '../src/set'
+import './set.demo.css'
 
 export default {
   title: 'Data Structures|Set',
 }
 
-export const todo = () => (
-  <div>
-    <p>Coming soon!</p>
-  </div>
-)
+const Item = ({ value, className }) => {
+  return <div className={className}>{value}</div>
+}
+
+const generateOddOrEvenClassName = index =>
+  index % 2 === 0 ? 'item' : 'item alt'
+
+class SetVisualizer extends Component {
+  state = {
+    set: new Set(),
+    itemToAdd: '',
+    itemToRemove: '',
+  }
+
+  addItemInputRef = createRef()
+  removeItemInputRef = createRef()
+
+  handleItemToAddChange = e => {
+    this.setState({ itemToAdd: e.target.value })
+  }
+
+  addItem = e => {
+    e.preventDefault()
+    const { set, itemToAdd } = this.state
+    if (itemToAdd) {
+      set.add(itemToAdd)
+      this.setState(
+        { set, itemToAdd: '' },
+        () =>
+          this.addItemInputRef.current && this.addItemInputRef.current.focus()
+      )
+    }
+  }
+
+  handleItemToRemoveChange = e => {
+    this.setState({ itemToRemove: e.target.value })
+  }
+
+  removeItem = e => {
+    e.preventDefault()
+    const { set, itemToRemove } = this.state
+    set.remove(itemToRemove)
+    this.setState(
+      { set, itemToRemove: '' },
+      () =>
+        this.removeItemInputRef.current &&
+        this.removeItemInputRef.current.focus()
+    )
+  }
+
+  clearSet = () => {
+    const { set } = this.state
+    set.clear()
+    this.setState({ set })
+  }
+
+  render() {
+    const { set, itemToAdd, itemToRemove } = this.state
+
+    return (
+      <div className="setDemo">
+        <h1>Set Demo</h1>
+        <form onSubmit={this.addItem}>
+          <input
+            value={itemToAdd}
+            onChange={this.handleItemToAddChange}
+            ref={this.addItemInputRef}
+            className="ti newItemTextInput"
+          />
+          <button className="button outline" type="submit">
+            Add Item to Set
+          </button>
+        </form>
+        <form onSubmit={this.removeItem}>
+          <input
+            value={itemToRemove}
+            onChange={this.handleItemToRemoveChange}
+            ref={this.removeItemInputRef}
+            className="ti removeItemTextInput"
+          />
+          <button className="button outline" type="submit">
+            Remove Item from Set
+          </button>
+          <br />
+          <button
+            className="button outline"
+            type="button"
+            onClick={this.clearSet}
+          >
+            Clear Set
+          </button>
+        </form>
+        <div>
+          <p>Set Contents:</p>
+          <div className="setContainer">
+            {set.enumerate().map((value, index) => {
+              return (
+                <Item
+                  value={value}
+                  className={generateOddOrEvenClassName(index)}
+                  key={index}
+                />
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export const setVisualizer = () => <SetVisualizer />
