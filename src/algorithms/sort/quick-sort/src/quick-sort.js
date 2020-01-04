c5b3765cf0834fe1820ae489449c0febcaaa988e
Tyler Hawkins <thawkins@instructure.com>
2020-01-04 10:55:49 -0700

    adds quick sort implementation

--- src/algorithms/sort/quick-sort/src/quick-sort.js
+++ src/algorithms/sort/quick-sort/src/quick-sort.js
@@ -16,6 +16,123 @@
 
 // Space required: O(n) (because it operates directly on the input array)
 
-export const quickSort = (arr, showLogs) => {
-  // TODO: implement quick sort
+export const quickSort = (arr, left = 0, right = arr.length - 1, showLogs) => {
+  /* istanbul ignore next */
+  showLogs && console.log(`current array: ${arr.join(' ')}`)
+
+  if (arr.length < 2) {
+    /* istanbul ignore next */
+    showLogs &&
+      console.log('  array only has 0 or 1 items, so nothing to sort!')
+    return arr
+  }
+
+  const pivotIndex = partition(arr, left, right, showLogs) // index returned from partition
+
+  if (left < pivotIndex - 1) {
+    // more elements on the left side of the pivot
+    quickSort(arr, left, pivotIndex - 1, showLogs)
+  }
+
+  if (pivotIndex < right) {
+    // more elements on the right side of the pivot
+    quickSort(arr, pivotIndex, right, showLogs)
+  }
+
+  return arr
+}
+
+// swaps the position of two elements in an array
+const swap = (arr, leftIndex, rightIndex, showLogs) => {
+  /* istanbul ignore next */
+  showLogs &&
+    console.log(
+      `    swapping elements with values ${arr[leftIndex]} and ${arr[rightIndex]}`
+    )
+
+  const temp = arr[leftIndex]
+  arr[leftIndex] = arr[rightIndex]
+  arr[rightIndex] = temp
+}
+
+const partition = (arr, leftIndex, rightIndex, showLogs) => {
+  const pivotIndex = Math.floor((rightIndex + leftIndex) / 2)
+  const pivotElement = arr[pivotIndex]
+
+  /* istanbul ignore next */
+  showLogs && console.log(`  pivot element chosen is ${pivotElement}`)
+
+  let currentLeftIndex = leftIndex
+  let currentRightIndex = rightIndex
+
+  /* istanbul ignore next */
+  showLogs &&
+    console.log(
+      `  current left element: ${arr[currentLeftIndex]}, current right element: ${arr[currentRightIndex]}`
+    )
+
+  while (currentLeftIndex <= currentRightIndex) {
+    while (arr[currentLeftIndex] < pivotElement) {
+      /* istanbul ignore next */
+      showLogs &&
+        console.log(
+          `    left element ${
+            arr[currentLeftIndex]
+          } is less than ${pivotElement}, moving right by one element to ${
+            arr[currentLeftIndex + 1]
+          }`
+        )
+
+      currentLeftIndex++
+    }
+
+    /* istanbul ignore next */
+    showLogs &&
+      console.log(
+        `    left element ${arr[currentLeftIndex]} is greater than or equal to ${pivotElement}, so ${arr[currentLeftIndex]} is eligible to be swapped`
+      )
+
+    while (arr[currentRightIndex] > pivotElement) {
+      /* istanbul ignore next */
+      showLogs &&
+        console.log(
+          `    right element ${
+            arr[currentRightIndex]
+          } is greater than ${pivotElement}, moving left by one element to ${
+            arr[currentRightIndex - 1]
+          }`
+        )
+
+      currentRightIndex--
+    }
+
+    /* istanbul ignore next */
+    showLogs &&
+      console.log(
+        `    right element ${arr[currentRightIndex]} is less than or equal to ${pivotElement}, so ${arr[currentRightIndex]} is eligible to be swapped`
+      )
+
+    if (currentLeftIndex <= currentRightIndex) {
+      /* istanbul ignore next */
+      showLogs &&
+        console.log(
+          `    left element ${arr[currentLeftIndex]} is less than or equal to right element ${arr[currentRightIndex]}, so we can swap these two elements`
+        )
+
+      swap(arr, currentLeftIndex, currentRightIndex, showLogs)
+      currentLeftIndex++
+      currentRightIndex--
+
+      /* istanbul ignore next */
+      showLogs && console.log(`    resulting array: ${arr.join(' ')}`)
+    } else {
+      /* istanbul ignore next */
+      showLogs &&
+        console.log(
+          `    left element ${arr[currentLeftIndex]} is NOT less than or equal to right element ${arr[currentRightIndex]}, so we will NOT swap these two elements`
+        )
+    }
+  }
+
+  return currentLeftIndex
 }
--- src/algorithms/sort/quick-sort/src/quick-sort.test.js
+++ src/algorithms/sort/quick-sort/src/quick-sort.test.js
@@ -3,6 +3,59 @@ import { quickSort } from './quick-sort'
 const showLogs = process.env.SHOW_LOGS === '1'
 
 describe('quickSort', () => {
-  // TODO
-  it('passes', () => {})
+  it('correctly sorts an unsorted array', () => {
+    const arrayToSort = [3, 6, 2, 8, 9, 1, 4, 5, 7]
+    const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    expect(quickSort(arrayToSort, undefined, undefined, showLogs)).toEqual(
+      expectedResult
+    )
+  })
+
+  it('correctly sorts a mostly sorted array', () => {
+    const arrayToSort = [1, 2, 9, 3, 4, 5, 6, 7, 8]
+    const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    expect(quickSort(arrayToSort, undefined, undefined, showLogs)).toEqual(
+      expectedResult
+    )
+  })
+
+  it('correctly sorts an inversely sorted array', () => {
+    const arrayToSort = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+    const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    expect(quickSort(arrayToSort, undefined, undefined, showLogs)).toEqual(
+      expectedResult
+    )
+  })
+
+  it('correctly returns an already sorted array', () => {
+    const arrayToSort = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    expect(quickSort(arrayToSort, undefined, undefined, showLogs)).toEqual(
+      expectedResult
+    )
+  })
+
+  it('can handle an array with duplicates', () => {
+    const arrayToSort = [3, 6, 2, 8, 4, 4, 4, 4, 7]
+    const expectedResult = [2, 3, 4, 4, 4, 4, 6, 7, 8]
+    expect(quickSort(arrayToSort, undefined, undefined, showLogs)).toEqual(
+      expectedResult
+    )
+  })
+
+  it('can handle an empty array', () => {
+    const arrayToSort = []
+    const expectedResult = []
+    expect(quickSort(arrayToSort, undefined, undefined, showLogs)).toEqual(
+      expectedResult
+    )
+  })
+
+  it('can handle an array with only one item', () => {
+    const arrayToSort = [1]
+    const expectedResult = [1]
+    expect(quickSort(arrayToSort, undefined, undefined, showLogs)).toEqual(
+      expectedResult
+    )
+  })
 })
