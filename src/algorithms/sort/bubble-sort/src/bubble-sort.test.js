a44a05f7a21082497ae288ad575de7a1e4309e0b
Tyler Hawkins <thawkins@instructure.com>
2020-01-04 11:00:25 -0700

    adds additional test cases for sorting algorithms

--- src/algorithms/sort/bubble-sort/src/bubble-sort.test.js
+++ src/algorithms/sort/bubble-sort/src/bubble-sort.test.js
@@ -15,6 +15,12 @@ describe('bubbleSort', () => {
     expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
   })
 
+  it('correctly sorts an inversely sorted array', () => {
+    const arrayToSort = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+    const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+  })
+
   it('correctly returns an already sorted array', () => {
     const arrayToSort = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -24,6 +30,18 @@ describe('bubbleSort', () => {
   it('can handle an array with duplicates', () => {
     const arrayToSort = [3, 6, 2, 8, 4, 4, 4, 4, 7]
     const expectedResult = [2, 3, 4, 4, 4, 4, 6, 7, 8]
+    expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+  })
+
+  it('can handle an empty array', () => {
+    const arrayToSort = []
+    const expectedResult = []
+    expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+  })
+
+  it('can handle an array with only one item', () => {
+    const arrayToSort = [1]
+    const expectedResult = [1]
     expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
   })
 })
