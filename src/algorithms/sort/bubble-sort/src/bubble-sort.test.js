95bf952133bfddf34646aa8f7128bda6da1fa9a1
Tyler Hawkins <thawkins@instructure.com>
2020-01-05 14:49:09 -0700

    updates sorting test suites to include arrays with even and odd number of elements

--- src/algorithms/sort/bubble-sort/src/bubble-sort.test.js
+++ src/algorithms/sort/bubble-sort/src/bubble-sort.test.js
@@ -3,45 +3,79 @@ import { bubbleSort } from './bubble-sort'
 const showLogs = process.env.SHOW_LOGS === '1'
 
 describe('bubbleSort', () => {
-  it('correctly sorts an unsorted array', () => {
-    const arrayToSort = [3, 6, 2, 8, 9, 1, 4, 5, 7]
-    const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+  describe('odd number of elements', () => {
+    it('correctly sorts an unsorted array', () => {
+      const arrayToSort = [3, 6, 2, 8, 9, 1, 4, 5, 7]
+      const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+    })
+
+    it('correctly sorts a mostly sorted array', () => {
+      const arrayToSort = [1, 2, 9, 3, 4, 5, 6, 7, 8]
+      const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+    })
+
+    it('correctly sorts an inversely sorted array', () => {
+      const arrayToSort = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+      const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+    })
+
+    it('correctly returns an already sorted array', () => {
+      const arrayToSort = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+    })
+
+    it('can handle an array with duplicates', () => {
+      const arrayToSort = [3, 6, 2, 8, 4, 4, 4, 4, 7]
+      const expectedResult = [2, 3, 4, 4, 4, 4, 6, 7, 8]
+      expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+    })
+
+    it('can handle an array with only one item', () => {
+      const arrayToSort = [1]
+      const expectedResult = [1]
+      expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+    })
   })
 
-  it('correctly sorts a mostly sorted array', () => {
-    const arrayToSort = [1, 2, 9, 3, 4, 5, 6, 7, 8]
-    const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
-  })
+  describe('even number of elements', () => {
+    it('correctly sorts an unsorted array', () => {
+      const arrayToSort = [3, 6, 2, 8, 9, 1, 4, 5, 10, 7]
+      const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+    })
 
-  it('correctly sorts an inversely sorted array', () => {
-    const arrayToSort = [9, 8, 7, 6, 5, 4, 3, 2, 1]
-    const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
-  })
+    it('correctly sorts a mostly sorted array', () => {
+      const arrayToSort = [1, 2, 9, 3, 4, 5, 6, 7, 8, 10]
+      const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+    })
 
-  it('correctly returns an already sorted array', () => {
-    const arrayToSort = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
-  })
+    it('correctly sorts an inversely sorted array', () => {
+      const arrayToSort = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+      const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+    })
 
-  it('can handle an array with duplicates', () => {
-    const arrayToSort = [3, 6, 2, 8, 4, 4, 4, 4, 7]
-    const expectedResult = [2, 3, 4, 4, 4, 4, 6, 7, 8]
-    expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
-  })
+    it('correctly returns an already sorted array', () => {
+      const arrayToSort = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+    })
 
-  it('can handle an empty array', () => {
-    const arrayToSort = []
-    const expectedResult = []
-    expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
-  })
+    it('can handle an array with duplicates', () => {
+      const arrayToSort = [3, 6, 2, 8, 4, 4, 10, 4, 4, 7]
+      const expectedResult = [2, 3, 4, 4, 4, 4, 6, 7, 8, 10]
+      expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+    })
 
-  it('can handle an array with only one item', () => {
-    const arrayToSort = [1]
-    const expectedResult = [1]
-    expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+    it('can handle an empty array', () => {
+      const arrayToSort = []
+      const expectedResult = []
+      expect(bubbleSort(arrayToSort, showLogs)).toEqual(expectedResult)
+    })
   })
 })
